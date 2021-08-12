6d1c3d402e0a075fab341f23219d60def6564b29
Takas0522 <t_o_19880522@outlook.com>
2021-08-12 22:22:37 +0900

    実験2

--- api/Comical.Api/Functions/ComicData.cs
+++ api/Comical.Api/Functions/ComicData.cs
@@ -25,7 +25,7 @@ namespace Comical.Api
 
         [FunctionName("ComicData")]
         public async Task<IActionResult> GetComicData(
-            [HttpTrigger(AuthorizationLevel.System, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
 
