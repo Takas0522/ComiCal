e011543ee956abdf112bf4587f79606269ddda27
Takas0522 <t_o_19880522@outlook.com>
2023-05-04 11:17:21 +0900

    update material

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
 import { Observable } from 'rxjs';
 import { AppService } from './app.service';
 import { LicenseDialogComponent } from './components/license-dialog/license-dialog.component';
