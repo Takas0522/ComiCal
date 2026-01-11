2c6b77aa936ab8d6a875c6b6803ec77f67f006e6
Takas0522 <t_o_19880522@outlook.com>
2026-01-11 07:18:54 +0000

    fic

---
+++ src/ComiCal.Server/Comical.Api/Functions/ComicData.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Comical.Api.Services;
+using Comical.Api.Models;
+using Comical.Api.Util.Common;
+
+namespace Comical.Api
+{
+    public class ComicData
+    {
+        private readonly IComicService _comicService;
+        private readonly ILogger<ComicData> _logger;
+
+        public ComicData(
+            IComicService comicService,
+            ILogger<ComicData> logger
+        )
+        {
+            _comicService = comicService;
+            _logger = logger;
+        }
+
+        [Function("ComicData")]
+        public async Task<HttpResponseData> GetComicData(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "options", Route = "ComicData")] HttpRequestData req)
+        {
+            // Handle OPTIONS requests for preflight
+            if (req.Method.Equals("OPTIONS", StringComparison.OrdinalIgnoreCase))
+            {
+                return req.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+
+            return await FunctionExecutionHelper.ExecuteAsync(
+                req,
+                _logger,
+                async () =>
+                {
+                    var query = req.Query["fromdate"];
+                    var fromdate = DateTime.UtcNow.AddMonths(-1);
+                    if (!string.IsNullOrEmpty(query))
+                    {
+                        if (!DateTime.TryParse(query, out fromdate))
+                        {
+                            throw new InvalidOperationException($"Invalid date format: {query}");
+                        }
+                        fromdate = fromdate.ToUniversalTime();
+                    }
+
+                    var data = await req.ReadFromJsonAsync<GetComicsRequest>();
+                    if (data is null)
+                    {
+                        throw new InvalidOperationException("Request body cannot be null");
+                    }
+
+                    var comics = await _comicService.GetComicsAsync(data, fromdate);
+
+                    return await HttpResponseHelper.CreateOkResponseAsync(req, comics);
+                });
+        }
+    }
+}
